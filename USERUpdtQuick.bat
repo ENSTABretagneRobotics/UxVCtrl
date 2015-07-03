eb31d45ff2a679064addd42eac8339a2a0ecaa86
lebarsfa <fabrice.le_bars@ensta-bretagne.fr>
2015-07-03 22:03:21 +0200

    Added env_ENSTA_Bretagne_Stadium.txt

--- USERUpdtQuick.bat
+++ USERUpdtQuick.bat
@@ -6,45 +6,63 @@ copy /B /Y /Z .\Release\UxVCtrl.exe "%UDK_USER_PATH%\BUGGY_WORKSPACE\UxVCtrl.exe
 copy /B /Y /Z .\CHANGELOG.txt "%UDK_USER_PATH%\BUGGY_WORKSPACE\CHANGELOG.txt"
 copy /B /Y /Z .\ReadMe.txt "%UDK_USER_PATH%\BUGGY_WORKSPACE\ReadMe.txt"
 copy /B /Y /Z .\mission_spec.txt "%UDK_USER_PATH%\BUGGY_WORKSPACE\mission_spec.txt"
+robocopy /E /ZB /PURGE /IS /IT /IOFF /ITEM /ETA /R:1 /W:2 /A-:R samples "%UDK_USER_PATH%\BUGGY_WORKSPACE\samples"\
+robocopy /E /ZB /PURGE /IS /IT /IOFF /ITEM /ETA /R:1 /W:2 /A-:R restore "%UDK_USER_PATH%\BUGGY_WORKSPACE\restore"\
 
 copy /B /Y /Z .\Release\UxVCtrl.exe "%UDK_USER_PATH%\CISSAU_WORKSPACE\UxVCtrl.exe"
 copy /B /Y /Z .\CHANGELOG.txt "%UDK_USER_PATH%\CISSAU_WORKSPACE\CHANGELOG.txt"
 copy /B /Y /Z .\ReadMe.txt "%UDK_USER_PATH%\CISSAU_WORKSPACE\ReadMe.txt"
 copy /B /Y /Z .\mission_spec.txt "%UDK_USER_PATH%\CISSAU_WORKSPACE\mission_spec.txt"
+robocopy /E /ZB /PURGE /IS /IT /IOFF /ITEM /ETA /R:1 /W:2 /A-:R samples "%UDK_USER_PATH%\CISSAU_WORKSPACE\samples"\
+robocopy /E /ZB /PURGE /IS /IT /IOFF /ITEM /ETA /R:1 /W:2 /A-:R restore "%UDK_USER_PATH%\CISSAU_WORKSPACE\restore"\
 
 copy /B /Y /Z .\Release\UxVCtrl.exe "%UDK_USER_PATH%\HOVERCRAFT_WORKSPACE\UxVCtrl.exe"
 copy /B /Y /Z .\CHANGELOG.txt "%UDK_USER_PATH%\HOVERCRAFT_WORKSPACE\CHANGELOG.txt"
 copy /B /Y /Z .\ReadMe.txt "%UDK_USER_PATH%\HOVERCRAFT_WORKSPACE\ReadMe.txt"
 copy /B /Y /Z .\mission_spec.txt "%UDK_USER_PATH%\HOVERCRAFT_WORKSPACE\mission_spec.txt"
+robocopy /E /ZB /PURGE /IS /IT /IOFF /ITEM /ETA /R:1 /W:2 /A-:R samples "%UDK_USER_PATH%\HOVERCRAFT_WORKSPACE\samples"\
+robocopy /E /ZB /PURGE /IS /IT /IOFF /ITEM /ETA /R:1 /W:2 /A-:R restore "%UDK_USER_PATH%\HOVERCRAFT_WORKSPACE\restore"\
 
 copy /B /Y /Z .\Release\UxVCtrl.exe "%UDK_USER_PATH%\MOTORBOAT_WORKSPACE\UxVCtrl.exe"
 copy /B /Y /Z .\CHANGELOG.txt "%UDK_USER_PATH%\MOTORBOAT_WORKSPACE\CHANGELOG.txt"
 copy /B /Y /Z .\ReadMe.txt "%UDK_USER_PATH%\MOTORBOAT_WORKSPACE\ReadMe.txt"
 copy /B /Y /Z .\mission_spec.txt "%UDK_USER_PATH%\MOTORBOAT_WORKSPACE\mission_spec.txt"
+robocopy /E /ZB /PURGE /IS /IT /IOFF /ITEM /ETA /R:1 /W:2 /A-:R samples "%UDK_USER_PATH%\MOTORBOAT_WORKSPACE\samples"\
+robocopy /E /ZB /PURGE /IS /IT /IOFF /ITEM /ETA /R:1 /W:2 /A-:R restore "%UDK_USER_PATH%\MOTORBOAT_WORKSPACE\restore"\
 
 copy /B /Y /Z .\Release\UxVCtrl.exe "%UDK_USER_PATH%\SAILBOAT_WORKSPACE\UxVCtrl.exe"
 copy /B /Y /Z .\CHANGELOG.txt "%UDK_USER_PATH%\SAILBOAT_WORKSPACE\CHANGELOG.txt"
 copy /B /Y /Z .\ReadMe.txt "%UDK_USER_PATH%\SAILBOAT_WORKSPACE\ReadMe.txt"
 copy /B /Y /Z .\mission_spec.txt "%UDK_USER_PATH%\SAILBOAT_WORKSPACE\mission_spec.txt"
+robocopy /E /ZB /PURGE /IS /IT /IOFF /ITEM /ETA /R:1 /W:2 /A-:R samples "%UDK_USER_PATH%\SAILBOAT_WORKSPACE\samples"\
+robocopy /E /ZB /PURGE /IS /IT /IOFF /ITEM /ETA /R:1 /W:2 /A-:R restore "%UDK_USER_PATH%\SAILBOAT_WORKSPACE\restore"\
 
 copy /B /Y /Z .\Release\UxVCtrl.exe "%UDK_USER_PATH%\SARDINE_WORKSPACE\UxVCtrl.exe"
 copy /B /Y /Z .\CHANGELOG.txt "%UDK_USER_PATH%\SARDINE_WORKSPACE\CHANGELOG.txt"
 copy /B /Y /Z .\ReadMe.txt "%UDK_USER_PATH%\SARDINE_WORKSPACE\ReadMe.txt"
 copy /B /Y /Z .\mission_spec.txt "%UDK_USER_PATH%\SARDINE_WORKSPACE\mission_spec.txt"
+robocopy /E /ZB /PURGE /IS /IT /IOFF /ITEM /ETA /R:1 /W:2 /A-:R samples "%UDK_USER_PATH%\SARDINE_WORKSPACE\samples"\
+robocopy /E /ZB /PURGE /IS /IT /IOFF /ITEM /ETA /R:1 /W:2 /A-:R restore "%UDK_USER_PATH%\SARDINE_WORKSPACE\restore"\
 
 copy /B /Y /Z .\Release\UxVCtrl.exe "%UDK_USER_PATH%\SAUCISSE_WORKSPACE\UxVCtrl.exe"
 copy /B /Y /Z .\CHANGELOG.txt "%UDK_USER_PATH%\SAUCISSE_WORKSPACE\CHANGELOG.txt"
 copy /B /Y /Z .\ReadMe.txt "%UDK_USER_PATH%\SAUCISSE_WORKSPACE\ReadMe.txt"
 copy /B /Y /Z .\mission_spec.txt "%UDK_USER_PATH%\SAUCISSE_WORKSPACE\mission_spec.txt"
+robocopy /E /ZB /PURGE /IS /IT /IOFF /ITEM /ETA /R:1 /W:2 /A-:R samples "%UDK_USER_PATH%\SAUCISSE_WORKSPACE\samples"\
+robocopy /E /ZB /PURGE /IS /IT /IOFF /ITEM /ETA /R:1 /W:2 /A-:R restore "%UDK_USER_PATH%\SAUCISSE_WORKSPACE\restore"\
 
 copy /B /Y /Z .\Release\UxVCtrl.exe "%UDK_USER_PATH%\SIMULATOR_WORKSPACE\UxVCtrl.exe"
 copy /B /Y /Z .\CHANGELOG.txt "%UDK_USER_PATH%\SIMULATOR_WORKSPACE\CHANGELOG.txt"
 copy /B /Y /Z .\ReadMe.txt "%UDK_USER_PATH%\SIMULATOR_WORKSPACE\ReadMe.txt"
 copy /B /Y /Z .\mission_spec.txt "%UDK_USER_PATH%\SIMULATOR_WORKSPACE\mission_spec.txt"
+robocopy /E /ZB /PURGE /IS /IT /IOFF /ITEM /ETA /R:1 /W:2 /A-:R samples "%UDK_USER_PATH%\SIMULATOR_WORKSPACE\samples"\
+robocopy /E /ZB /PURGE /IS /IT /IOFF /ITEM /ETA /R:1 /W:2 /A-:R restore "%UDK_USER_PATH%\SIMULATOR_WORKSPACE\restore"\
 
 copy /B /Y /Z .\Release\UxVCtrl.exe "%UDK_USER_PATH%\VAIMOS_WORKSPACE\UxVCtrl.exe"
 copy /B /Y /Z .\CHANGELOG.txt "%UDK_USER_PATH%\VAIMOS_WORKSPACE\CHANGELOG.txt"
 copy /B /Y /Z .\ReadMe.txt "%UDK_USER_PATH%\VAIMOS_WORKSPACE\ReadMe.txt"
 copy /B /Y /Z .\mission_spec.txt "%UDK_USER_PATH%\VAIMOS_WORKSPACE\mission_spec.txt"
+robocopy /E /ZB /PURGE /IS /IT /IOFF /ITEM /ETA /R:1 /W:2 /A-:R samples "%UDK_USER_PATH%\VAIMOS_WORKSPACE\samples"\
+robocopy /E /ZB /PURGE /IS /IT /IOFF /ITEM /ETA /R:1 /W:2 /A-:R restore "%UDK_USER_PATH%\VAIMOS_WORKSPACE\restore"\
 
 pause
