ad2114775706973f0fa282da35d9bc1a60d37e98
lebarsfa <fabrice.le_bars@ensta-bretagne.fr>
2015-08-29 21:58:28 +0200

    Sailboat configuration

--- UxVCtrl_Zip.bat
+++ UxVCtrl_Zip.bat
@@ -1,4 +1,4 @@
 cd /d "%~dp0"
-7z a -y "..\UxVCtrl_Zip.zip" "..\UxVCtrl" "..\OSUtils" "..\Extensions" "..\interval" "..\matrix_lib" -xr!"Debug" -xr!"Release" -xr!"UxVCtrl.ncb" -xr!"UxVCtrl.sdf"
+7z a -y "..\UxVCtrl_Zip.zip" "..\UxVCtrl" "..\OSUtils" "..\Extensions" "..\interval" "..\matrix_lib" -xr!"Debug" -xr!"Release" -xr!"UxVCtrl.ncb" -xr!"UxVCtrl.sdf" -xr!"dummy_25min.wmv" -xr!"test1.wmv" -xr!"test2.wmv" -xr!"test3.wmv"
 pause
 exit
