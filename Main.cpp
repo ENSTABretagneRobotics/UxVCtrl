8c689d032f5dee3e994f16cca6f4b6dcb4c6e481
lebarsfa <fabrice.le_bars@ensta-bretagne.org>
2022-06-19 19:03:09 +0200

    Updated project files to use OpenCV 4.2.0 as default

--- Main.cpp
+++ Main.cpp
@@ -227,7 +227,7 @@ int main(int argc, char* argv[])
 
 	srand(GetTickCount());
 
-	printf("\nUxVCtrl V396\n");
+	printf("\nUxVCtrl V397\n");
 	fflush(stdout);
 
 	// Will launch a mission file if specified as argument.
