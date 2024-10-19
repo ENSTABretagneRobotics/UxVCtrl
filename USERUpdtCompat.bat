828eeb969eabb39333f59019a8d55fe14ae70dc6
lebarsfa <fabrice.le_bars@ensta-bretagne.org>
2024-10-19 16:51:55 +0200

    Update USERUpdtCompat.bat

--- USERUpdtCompat.bat
+++ USERUpdtCompat.bat
@@ -12,17 +12,20 @@ move /Y %USERPROFILE%\Downloads\UxVCtrl_cv2413.exe "%UDK_USER_PATH%\Compatibilit
 move /Y %USERPROFILE%\Downloads\UxVCtrl_x64.exe "%UDK_USER_PATH%\Compatibility mode\Windows_x64\"
 move /Y %USERPROFILE%\Downloads\UxVCtrl_mingw11_x64.exe "%UDK_USER_PATH%\Compatibility mode\Windows_x64\"
 
+move /Y %USERPROFILE%\Downloads\UxVCtrl_compat_ubuntu_noble "%UDK_USER_PATH%\Compatibility mode\Ubuntu\"
+move /Y %USERPROFILE%\Downloads\UxVCtrl_ubuntu_noble "%UDK_USER_PATH%\Compatibility mode\Ubuntu\"
 move /Y %USERPROFILE%\Downloads\UxVCtrl_compat_ubuntu_jammy "%UDK_USER_PATH%\Compatibility mode\Ubuntu\"
 move /Y %USERPROFILE%\Downloads\UxVCtrl_ubuntu_jammy "%UDK_USER_PATH%\Compatibility mode\Ubuntu\"
+
 move /Y %USERPROFILE%\Downloads\UxVCtrl_compat_ubuntu_focal "%UDK_USER_PATH%\Compatibility mode\Ubuntu\"
 move /Y %USERPROFILE%\Downloads\UxVCtrl_ubuntu_focal "%UDK_USER_PATH%\Compatibility mode\Ubuntu\"
-
+move /Y %USERPROFILE%\Downloads\UxVCtrl_compat_ubuntu_bionic "%UDK_USER_PATH%\Compatibility mode\Ubuntu\"
 move /Y %USERPROFILE%\Downloads\UxVCtrl_compat_ubuntu_xenial_i386 "%UDK_USER_PATH%\Compatibility mode\Ubuntu_i386\"
 
-move /Y %USERPROFILE%\Downloads\UxVCtrl_compat_buster_pi "%UDK_USER_PATH%\Compatibility mode\pi\"
-move /Y %USERPROFILE%\Downloads\UxVCtrl_buster_pi "%UDK_USER_PATH%\Compatibility mode\pi\"
+move /Y %USERPROFILE%\Downloads\UxVCtrl_compat_bookworm_pi "%UDK_USER_PATH%\Compatibility mode\pi\"
+move /Y %USERPROFILE%\Downloads\UxVCtrl_bookworm_pi "%UDK_USER_PATH%\Compatibility mode\pi\"
 
-move /Y %USERPROFILE%\Downloads\UxVCtrl_compat_macos_monterey "%UDK_USER_PATH%\Compatibility mode\macOS\"
-move /Y %USERPROFILE%\Downloads\UxVCtrl_macos_monterey "%UDK_USER_PATH%\Compatibility mode\macOS\"
+move /Y %USERPROFILE%\Downloads\UxVCtrl_compat_macos_ventura "%UDK_USER_PATH%\Compatibility mode\macOS\"
+move /Y %USERPROFILE%\Downloads\UxVCtrl_macos_ventura "%UDK_USER_PATH%\Compatibility mode\macOS\"
 
 pause
