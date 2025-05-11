65676c4f6ae523903e544e84d3d82823d4251588
lebarsfa <fabrice.le_bars@ensta-bretagne.org>
2025-05-11 13:06:35 +0200

    Update ResetWorkspace.bat

--- ResetWorkspace.bat
+++ ResetWorkspace.bat
@@ -8,8 +8,8 @@ md log
 md pic
 md vid
 md aud
-copy /Y /Z restore\log\ReadMe.txt log\ReadMe.txt
-copy /Y /Z restore\pic\ReadMe.txt pic\ReadMe.txt
-copy /Y /Z restore\vid\ReadMe.txt vid\ReadMe.txt
-copy /Y /Z restore\aud\ReadMe.txt aud\ReadMe.txt
+copy /B /Y /Z restore\log\ReadMe.txt log\ReadMe.txt
+copy /B /Y /Z restore\pic\ReadMe.txt pic\ReadMe.txt
+copy /B /Y /Z restore\vid\ReadMe.txt vid\ReadMe.txt
+copy /B /Y /Z restore\aud\ReadMe.txt aud\ReadMe.txt
 pause
