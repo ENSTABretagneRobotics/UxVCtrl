5dba1ffe28bd58cb87524109b5f39c0d671b11d8
lebarsfa <fabrice.le_bars@ensta.fr>
2025-12-07 19:26:55 +0100

    Improve support of CMake 4 and update workflows

--- USERUpdtCompat.bat
+++ USERUpdtCompat.bat
@@ -5,33 +5,27 @@ set UDK_USER_PATH=..\..\..\..\..\..\..\..\Robotics data\Competitions or robots s
 move /Y %USERPROFILE%\Downloads\UxVCtrl_compat.exe "%UDK_USER_PATH%\Compatibility mode\Windows\"
 move /Y %USERPROFILE%\Downloads\UxVCtrl.exe "%UDK_USER_PATH%\Compatibility mode\Windows\"
 move /Y %USERPROFILE%\Downloads\UxVCtrl_alt.exe "%UDK_USER_PATH%\Compatibility mode\Windows\"
-move /Y %USERPROFILE%\Downloads\UxVCtrl_mingw11_x86.exe "%UDK_USER_PATH%\Compatibility mode\Windows\"
+move /Y %USERPROFILE%\Downloads\UxVCtrl_mingw13_x86.exe "%UDK_USER_PATH%\Compatibility mode\Windows\"
 move /Y %USERPROFILE%\Downloads\UxVCtrl_cv320_ffmpeg.exe "%UDK_USER_PATH%\Compatibility mode\Windows\"
 move /Y %USERPROFILE%\Downloads\UxVCtrl_cv2413.exe "%UDK_USER_PATH%\Compatibility mode\Windows\"
 
 move /Y %USERPROFILE%\Downloads\UxVCtrl_x64.exe "%UDK_USER_PATH%\Compatibility mode\Windows_x64\"
-move /Y %USERPROFILE%\Downloads\UxVCtrl_mingw11_x64.exe "%UDK_USER_PATH%\Compatibility mode\Windows_x64\"
+move /Y %USERPROFILE%\Downloads\UxVCtrl_mingw13_x64.exe "%UDK_USER_PATH%\Compatibility mode\Windows_x64\"
 
 move /Y %USERPROFILE%\Downloads\UxVCtrl_compat_ubuntu_noble "%UDK_USER_PATH%\Compatibility mode\Ubuntu\"
 move /Y %USERPROFILE%\Downloads\UxVCtrl_ubuntu_noble "%UDK_USER_PATH%\Compatibility mode\Ubuntu\"
 move /Y %USERPROFILE%\Downloads\UxVCtrl_compat_ubuntu_jammy "%UDK_USER_PATH%\Compatibility mode\Ubuntu\"
 move /Y %USERPROFILE%\Downloads\UxVCtrl_ubuntu_jammy "%UDK_USER_PATH%\Compatibility mode\Ubuntu\"
 
-move /Y %USERPROFILE%\Downloads\UxVCtrl_compat_ubuntu_focal "%UDK_USER_PATH%\Compatibility mode\Ubuntu\"
-move /Y %USERPROFILE%\Downloads\UxVCtrl_ubuntu_focal "%UDK_USER_PATH%\Compatibility mode\Ubuntu\"
-move /Y %USERPROFILE%\Downloads\UxVCtrl_compat_ubuntu_bionic "%UDK_USER_PATH%\Compatibility mode\Ubuntu\"
-
 move /Y %USERPROFILE%\Downloads\UxVCtrl_compat_ubuntu_noble_arm64 "%UDK_USER_PATH%\Compatibility mode\Ubuntu_arm64\"
 move /Y %USERPROFILE%\Downloads\UxVCtrl_ubuntu_noble_arm64 "%UDK_USER_PATH%\Compatibility mode\Ubuntu_arm64\"
 move /Y %USERPROFILE%\Downloads\UxVCtrl_compat_ubuntu_jammy_arm64 "%UDK_USER_PATH%\Compatibility mode\Ubuntu_arm64\"
 move /Y %USERPROFILE%\Downloads\UxVCtrl_ubuntu_jammy_arm64 "%UDK_USER_PATH%\Compatibility mode\Ubuntu_arm64\"
 
-move /Y %USERPROFILE%\Downloads\UxVCtrl_compat_ubuntu_xenial_i386 "%UDK_USER_PATH%\Compatibility mode\Ubuntu_i386\"
-
 move /Y %USERPROFILE%\Downloads\UxVCtrl_compat_bookworm_pi "%UDK_USER_PATH%\Compatibility mode\pi\"
 move /Y %USERPROFILE%\Downloads\UxVCtrl_bookworm_pi "%UDK_USER_PATH%\Compatibility mode\pi\"
 
-move /Y %USERPROFILE%\Downloads\UxVCtrl_compat_macos_ventura "%UDK_USER_PATH%\Compatibility mode\macOS\"
-move /Y %USERPROFILE%\Downloads\UxVCtrl_macos_ventura "%UDK_USER_PATH%\Compatibility mode\macOS\"
+move /Y %USERPROFILE%\Downloads\UxVCtrl_compat_macos_sonoma_x86_64 "%UDK_USER_PATH%\Compatibility mode\macOS\"
+move /Y %USERPROFILE%\Downloads\UxVCtrl_macos_sonoma_x86_64 "%UDK_USER_PATH%\Compatibility mode\macOS\"
 
 pause
